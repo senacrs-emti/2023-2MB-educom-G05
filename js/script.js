a2d842bafb287a37f15ad9c5d3631605b59cd44b
viniross <108466053+viniross@users.noreply.github.com>
2023-11-09 12:26:52 -0300

    calc funcionando

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*
 const previousOperationText = document.querySelector("#previous-operation");
 const currentOperationText = document.querySelector("#current-operation");
 const buttons = document.querySelectorAll("#buttons-container button");
@@ -147,3 +148,105 @@ buttons.forEach((btn) => {
         }
     });
 });
+*/
+const previousOperationText = document.querySelector("#previous-operation");
+const currentOperationText = document.querySelector("#current-operation");
+
+let num1 = 0;
+let num2 = 0;
+
+let isSetNum1 = false;
+let isSetNum2 = false;
+
+let operator = 0;
+
+function number(num) {
+    num2 *= 10;
+    num2 += num;
+
+    isSetNum2 = true;
+
+    currentOperationText.innerHTML = num2;
+
+    updateprevious();
+}
+
+function executeOp() {
+    if (!isSetNum1 && isSetNum2) {
+        num1 = num2;
+        num2 = 0;
+        isSetNum1 = true;
+        isSetNum2 = false;
+    }
+    else if (isSetNum1 && isSetNum2) {
+        switch (operator) {
+            case 1: {
+                num1 += num2;
+                num2 = 0;
+                isSetNum2 = false;
+                break;
+            }
+            case 2: {
+                num1 -= num2;
+                num2 = 0;
+                isSetNum2 = false;
+                break;
+            }
+            case 3: {
+                num1 *= num2;
+                num2 = 0;
+                isSetNum2 = false;
+                break;
+            }
+            case 4: {
+                num1 /= num2;
+                num2 = 0;
+                isSetNum2 = false;
+                break;
+            }
+            default: break;
+        }
+
+        currentOperationText.innerHTML = num1;
+    }
+}
+
+function charop() {
+    switch (operator) {
+        case 1: return ' + ';
+        case 2: return ' - ';
+        case 3: return ' × ';
+        case 4: return ' ÷ ';
+        default: return ' ';
+    }
+}
+
+function updateprevious() {
+    if (!isSetNum1) {
+        previousOperationText.innerHTML = num2;
+        return;
+    }
+
+    let char = charop();
+
+    if (char == ' ') return;
+
+    if (!isSetNum2) {
+        previousOperationText.innerHTML = num1 + char;
+        return;
+    }
+
+    
+    previousOperationText.innerHTML = num1 + char + num2;
+}
+
+function operations(op) {
+    executeOp();
+
+    if (op <= 4) operator = op;
+    else operator = 0;
+
+    updateprevious();
+
+    //alert("Num1: " + num1 + " --- Num2: " + num2 + " --- Operator: " + operator);
+}
